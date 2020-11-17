c04b6a40150fd7247456fee8e7e607c565402417
Alexis Bierent <alexis.bierent@gmail.com>
2020-11-17 14:16:00 +0100

    rails new

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,13 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+    def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+end
